8dad9d3c471b95ec4326617ba99004c548c281be
odri19995 <odri19995@gmali.com>
2023-06-02 18:21:14 +0900

    충돌문제 해결

--- src/main/java/com/example/project/controller/UsrHomeController.java
+++ src/main/java/com/example/project/controller/UsrHomeController.java
@@ -27,88 +27,90 @@ public class UsrHomeController {
 
 	@GetMapping("/usr/home/main")
 	public String kakaoCallback(HttpServletRequest request, @RequestParam(required=false)String code) {
-		
-		
-		RestTemplate rt = new RestTemplate();
-		
-		
-		// HttpHeader 오브젝트 생성
-		HttpHeaders headers = new HttpHeaders();
-		headers.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
-		
-		//HttpBody 오브젝트 생성
-		MultiValueMap<String,String> params = new LinkedMultiValueMap<>();
-		params.add("grant_type", "authorization_code");
-		params.add("client_id", "ef68c2f02e7c9f1813716129dfbdccf5");
-		params.add("redirect_uri", "http://localhost:8081/usr/home/main");
-		params.add("code", code);
-		
-		//HttpHeader와 HttpBody를 하나의 오브젝트에 담기
-		HttpEntity<MultiValueMap<String,String>> kakaoTokenRequest =
-			new HttpEntity<>(params,headers);
-		
-		// Http 요청하기 - post방식으로 -그리고 response변수의 응답을 받음
-		ResponseEntity<String> response = rt.exchange(
-				"https://kauth.kakao.com/oauth/token",
-				HttpMethod.POST,
-				kakaoTokenRequest,
-				String.class				
-		);
-		
-		// GSon, Json Simple, ObjectMapper();
-		
-		ObjectMapper objectMapper = new ObjectMapper();
-		OAuthToken oauthToken = null;
-		
-		try {
-			oauthToken = objectMapper.readValue(response.getBody(),OAuthToken.class);
-		} catch (JsonProcessingException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		if(code != null) {
+			
+			RestTemplate rt = new RestTemplate();
+			
+			
+			// HttpHeader 오브젝트 생성
+			HttpHeaders headers = new HttpHeaders();
+			headers.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
+			
+			//HttpBody 오브젝트 생성
+			MultiValueMap<String,String> params = new LinkedMultiValueMap<>();
+			params.add("grant_type", "authorization_code");
+			params.add("client_id", "ef68c2f02e7c9f1813716129dfbdccf5");
+			params.add("redirect_uri", "http://localhost:8081/usr/home/main");
+			params.add("code", code);
+			
+			//HttpHeader와 HttpBody를 하나의 오브젝트에 담기
+			HttpEntity<MultiValueMap<String,String>> kakaoTokenRequest =
+				new HttpEntity<>(params,headers);
+			
+			// Http 요청하기 - post방식으로 -그리고 response변수의 응답을 받음
+			ResponseEntity<String> response = rt.exchange(
+					"https://kauth.kakao.com/oauth/token",
+					HttpMethod.POST,
+					kakaoTokenRequest,
+					String.class				
+			);
+			
+			// GSon, Json Simple, ObjectMapper();
+			
+			ObjectMapper objectMapper = new ObjectMapper();
+			OAuthToken oauthToken = null;
+			
+			try {
+				oauthToken = objectMapper.readValue(response.getBody(),OAuthToken.class);
+			} catch (JsonProcessingException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			System.out.println("카카오 엑세스 토큰 : " + oauthToken.getAccess_token());
+	
+			
+			RestTemplate rt2 = new RestTemplate();
+			
+			
+			// HttpHeader 오브젝트 생성
+			HttpHeaders headers2 = new HttpHeaders();
+			headers2.add("Authorization", "Bearer "+oauthToken.getAccess_token());
+			headers2.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
+					
+			//HttpHeader와 HttpBody를 하나의 오브젝트에 담기
+			HttpEntity<MultiValueMap<String,String>> kakaoProfileRequest =
+				new HttpEntity<>(headers2);
+			
+			// Http 요청하기 - post방식으로 -그리고 response변수의 응답을 받음
+			ResponseEntity<String> response2 = rt2.exchange(
+					"https://kapi.kakao.com/v2/user/me",
+					HttpMethod.POST,
+					kakaoProfileRequest,
+					String.class				
+			);
+			System.out.println(response2.getBody());
+			
+			ObjectMapper objectMapper2 = new ObjectMapper();
+			KakaoProfile kakaoProfile = null;
+			
+			try {
+				kakaoProfile = objectMapper2.readValue(response2.getBody(),KakaoProfile.class);
+			} catch (JsonProcessingException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			System.out.println("카카오 아이디 번호 : " +kakaoProfile.getId());
+			System.out.println("카카오 사용자 : " +kakaoProfile.getProperties().getNickname());
+	
+			// 2-2. id와 pwd가 일치하면,
+			HttpSession session = request.getSession();
+			
+			// 세션 객체에 id를 저장		
+			session.setAttribute("id",kakaoProfile.getId());
+			return "usr/home/main";
 		}
-		
-		System.out.println("카카오 엑세스 토큰 : " + oauthToken.getAccess_token());
-
-		
-		RestTemplate rt2 = new RestTemplate();
-		
-		
-		// HttpHeader 오브젝트 생성
-		HttpHeaders headers2 = new HttpHeaders();
-		headers2.add("Authorization", "Bearer "+oauthToken.getAccess_token());
-		headers2.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
-				
-		//HttpHeader와 HttpBody를 하나의 오브젝트에 담기
-		HttpEntity<MultiValueMap<String,String>> kakaoProfileRequest =
-			new HttpEntity<>(headers2);
-		
-		// Http 요청하기 - post방식으로 -그리고 response변수의 응답을 받음
-		ResponseEntity<String> response2 = rt2.exchange(
-				"https://kapi.kakao.com/v2/user/me",
-				HttpMethod.POST,
-				kakaoProfileRequest,
-				String.class				
-		);
-		System.out.println(response2.getBody());
-		
-		ObjectMapper objectMapper2 = new ObjectMapper();
-		KakaoProfile kakaoProfile = null;
-		
-		try {
-			kakaoProfile = objectMapper2.readValue(response2.getBody(),KakaoProfile.class);
-		} catch (JsonProcessingException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		System.out.println("카카오 아이디 번호 : " +kakaoProfile.getId());
-		System.out.println("카카오 사용자 : " +kakaoProfile.getProperties().getNickname());
-
-		// 2-2. id와 pwd가 일치하면,
-		HttpSession session = request.getSession();
-		
-		// 세션 객체에 id를 저장		
-		session.setAttribute("id",kakaoProfile.getId());
 
 		
 		return "usr/home/main";
--- src/main/webapp/WEB-INF/jsp/usr/home/boardList.jsp
+++ src/main/webapp/WEB-INF/jsp/usr/home/boardList.jsp
@@ -5,13 +5,13 @@
 <html>
 <head>
 	<meta charset="UTF-8">
-    <title>fastcampus</title>
+    <title>boardpage</title>
     <link rel="stylesheet" href="<c:url value='/css/menu.css'/>">
 </head>
 <body>
 <div id="menu">
 	<ul>
-	    <li id="logo">fastcampus</li>
+	    <li id="logo">boardpage</li>
 	    <li><a href="<c:url value='/'/>">Home</a></li>
 	    <li><a href="<c:url value='/board/list'/>">Board</a></li>
 		<c:if test="${sessionScope.id == null }">
